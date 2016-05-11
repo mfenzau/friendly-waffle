c6f95ce154bd86c873232ff4eabc63b7b173fe00
mfenzau <ohhthtsmiranda@gmail.com>
2016-05-11 20:59:01 +0000

    new label

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
-
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class Product < ActiveRecord::Base
+  validates :title, :description, :img_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :img_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
